84c5d1d82bd4a89cf9ba28652fccd486b8fc86cb
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-23 06:01:35 +0530

    display: Make reserved_size in gralloc handle optional
    Few devices that use older stock blobs don't support this yet.
    Allow them to disable it via setting
    TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := false
    in BoardConfig, otherwise enabled by default.
    
    Change-Id: I7c62ef6e7d89f1b2d7b9080e8d3e7e2d09f6316b
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/display-product.mk
+++ config/display-product.mk
@@ -188,15 +188,21 @@ endif
 SOONG_CONFIG_NAMESPACES += qtidisplay
 
 # Soong Keys
-SOONG_CONFIG_qtidisplay := drmpp headless llvmsa gralloc4 udfps default
+SOONG_CONFIG_qtidisplay := drmpp headless llvmsa gralloc4 gralloc_handle_has_reserved_size udfps default
 
 # Soong Values
 SOONG_CONFIG_qtidisplay_drmpp := true
 SOONG_CONFIG_qtidisplay_headless := false
 SOONG_CONFIG_qtidisplay_llvmsa := false
 SOONG_CONFIG_qtidisplay_gralloc4 := true
+
+SOONG_CONFIG_qtidisplay_gralloc_handle_has_reserved_size := true
 SOONG_CONFIG_qtidisplay_udfps := false
 SOONG_CONFIG_qtidisplay_default := true
+
+ifeq ($(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE),false)
+    SOONG_CONFIG_qtidisplay_gralloc_handle_has_reserved_size := false
+endif
 
 ifeq ($(TARGET_IS_HEADLESS), true)
     PRODUCT_SOONG_NAMESPACES += $(DISPLAY_HAL_DIR)/qmaa
